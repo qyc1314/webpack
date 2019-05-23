7db8761d63a4711f3ad7b999caf23301d8be5090
qyc <qinyongchao@banggood.com>
2019-05-23 15:18:43 +0800

    优化加入了接口封装，线上和测试环境等

--- template/src/config/fetch.js
+++ template/src/config/fetch.js
@@ -2,31 +2,45 @@
  * 接口封装方式.
  */
 import axios from 'axios';
-import qs from 'qs'
-import {
-  baseUrl
-} from './env';
+import qs from 'qs';
 
-export default async (url = '', data ={}, type = 'GET') => {
-  url = baseUrl + url;
+export default async (url='', data={}, type='GET',options={}) => {
   type = type.toLowerCase();
 
   const config = {
     method:type,
     url:url,
   };
-  if(type === 'get'){
-    config.params = data;
-  }else{
-    config.data = qs.stringify(data);
-  }
 
-  const response = await axios(config);
-  if(response.status === 200){
-    return response.data;
-  }else if(response.state === 401){
-    return null;
-  }else{
-    return null;
+  type === 'get'
+    ? config.params = data
+    : config.data = qs.stringify(data);
+
+  for(let i in options){
+    config[i]=options[i];
   }
+  console.log(config)
+  const response = await axios(config).then(res=>{
+    return{
+      success:true,
+      res:res
+    }
+  }).catch(function (error) {
+    return{
+      success:false,
+      code:error.response.status
+    }
+  });
+  return response
+
+  // if(response.status === 200){
+  //   return response.data;
+  //   if(response.data.status==-1){
+  //
+  //   }
+  // }else if(response.state === 401){
+  //   return null;
+  // }else{
+  //   return null;
+  // }
 }
