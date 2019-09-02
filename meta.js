62984fd1f7081bb5b85b19f374c4b6cd07903115
qyc <qinyongchao@banggood.com>
2019-09-02 17:03:04 +0800

    优化加入了接口封装，线上和测试环境等

--- meta.js
+++ meta.js
@@ -160,7 +160,7 @@ module.exports = {
   filters: {
     '.eslintrc.js': 'lint',
     '.eslintignore': 'lint',
-    'config/test.env.js': 'unit || e2e',
+    // 'config/test.env.js': 'unit || e2e',
     'build/webpack.test.conf.js': "unit && runner === 'karma'",
     'test/unit/**/*': 'unit',
     'test/unit/index.js': "unit && runner === 'karma'",
